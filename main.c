ebd12259ff134772254bb4ee49b561ae62f747f5
andrew osama <andrewosamayehia@gmail.com>
2024-09-15 21:03:50 +0300

    1.0.0v

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Hamada 10
+
+int main()
+{
+int y= 45;
+int x = Hamada;
+int z = 15;
+printf("x value = %i\n",x);
+printf("y value = %i\n",y);
+printf("z value = %i\n",z);
+return 0;
+}
